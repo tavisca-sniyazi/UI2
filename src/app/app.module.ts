9f9d256257aeb2f39f952e30469a15f915c35810
tavisca-sniyazi <51950508+tavisca-sniyazi@users.noreply.github.com>
2019-11-22 17:57:21 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginModuleComponent } from './login-module/login-module.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginModuleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
